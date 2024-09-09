b9563f167652c92488110e86cd3310cdc9e203fe
OmBhavsar161 <ombhavsar2004@gmail.com>
2024-09-09 09:41:43 +0530

    ii

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,7 @@ app.use(
 );
 
 // Serve static files from 'public' directory
-app.use("/images", express.static(path.join(__dirname, "upload/images")));
+app.use("/Images", express.static(path.join(__dirname, "Upload/Images")));
 
 
 mongoose

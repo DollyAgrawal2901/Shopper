59b083f846db3c247036f50b2d22709fcf93e45e
OmBhavsar161 <ombhavsar2004@gmail.com>
2024-09-09 16:59:52 +0530

    t

--- Backend/index.js
+++ Backend/index.js
@@ -130,7 +130,7 @@ app.post("/addproduct", async (req, res) => {
       id: id,
       name: req.body.name,
       image: req.body.image,
-      category: req.body.category,
+      category: req.body.category, 
       new_price: req.body.new_price,
       old_price: req.body.old_price,
     });
